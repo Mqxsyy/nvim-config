96ae89515d6fea0519464125ccc2c0ee31634bf0
Mqxsyy <gertmagi9@gmail.com>
2024-08-25 10:22:07 +0300

    transparent bg, alpha changes, relative line nr

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -6,6 +6,10 @@ vim.cmd("set shiftwidth=4")
 vim.g.mapleader = " "
 
 vim.opt.number = true
+vim.o.relativenumber = true
 
 vim.api.nvim_set_keymap('n', '<C-d>', '<C-d>zz', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<C-u>', '<C-u>zz', { noremap = true, silent = true })
+
+vim.api.nvim_set_keymap('n', 'n', 'nzz', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'N', 'Nzz', { noremap = true, silent = true })
