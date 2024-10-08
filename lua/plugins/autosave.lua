3b76b0fa8c032bf7c7d5abfea73dbfbc59615585
Mqxsyy <gertmagi9@gmail.com>
2024-10-08 08:37:31 +0300

    autosave

---
+++ lua/plugins/autosave.lua
@@ -0,0 +1,25 @@
+return {
+    "pocco81/auto-save.nvim",
+    config = function()
+        require("auto-save").setup({
+            enabled = true,
+            execution_message = {
+                message = function()
+                    return "AutoSave: saved file"
+                end,
+                dim = 0.18,
+                cleaning_interval = 1250,
+            },
+            condition = function(buf)
+                local fn = vim.fn
+                local utils = require("auto-save.utils.data")
+
+                if fn.getbufvar(buf, "&modifiable") == 1 and utils.not_in(fn.getbufvar(buf, "&filetype"), {}) then
+                    return true
+                end
+
+                return false
+            end,
+        })
+    end,
+}
