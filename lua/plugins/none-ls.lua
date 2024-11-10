929281c19c9b93d0e1cf8fd5176e2660c0f7044a
Mqxsyy <gertmagi9@gmail.com>
2024-11-10 18:02:53 +0200

    use eslint and prettier if biome config doesn't exist

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,11 +1,19 @@
+local function biome_config_exists()
+    return vim.fn.glob ".biome.json" ~= ""
+end
+
 return {
     "nvimtools/none-ls.nvim",
     config = function()
         local null_ls = require "null-ls"
 
-        null_ls.setup {
-            sources = {
-                null_ls.builtins.formatting.stylua,
+        local sources = {
+            null_ls.builtins.formatting.stylua,
+        }
+
+        if biome_config_exists() then
+            table.insert(
+                sources,
                 null_ls.builtins.formatting.biome.with {
                     args = {
                         "check",
@@ -13,8 +21,15 @@ return {
                         "--skip-errors",
                         "--stdin-file-path=$FILENAME",
                     },
-                },
-            },
+                }
+            )
+        else
+            table.insert(sources, null_ls.builtins.formatting.prettierd)
+            table.insert(sources, null_ls.builtins.diagnostics.eslint_d)
+        end
+
+        null_ls.setup {
+            sources = sources,
         }
 
         vim.keymap.set("n", "<leader>ff", vim.lsp.buf.format, {})
