82c79eb9412df73669147858c1d64604ba6c0d54
Mqxsyy <gertmagi9@gmail.com>
2024-11-28 12:27:17 +0200

    diabled swap files

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -4,6 +4,8 @@ vim.cmd "set softtabstop=4"
 vim.cmd "set shiftwidth=4"
 vim.cmd "set scrolloff=10"
 
+vim.opt.swapfile = false
+
 vim.cmd "set clipboard+=unnamedplus"
 vim.g.clipboard = {
 	name = "WslClipboard",
