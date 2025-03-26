ac7726c37505f874a89a0144fa1ca87c43cc0a03
Mqxsyy <gertmagi9@gmail.com>
2025-03-26 16:03:45 +0200

    moved some more keymaps and easier pane switching

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -2,6 +2,26 @@ local map = require "utils.map"
 
 local exported = {}
 
+--> Nvim
+map("i", "<C-h>", "<Left>")
+map("i", "<C-j>", "<Down>")
+map("i", "<C-k>", "<Up>")
+map("i", "<C-l>", "<Right>")
+
+map("n", "<C-h>", "<C-w>h")
+map("n", "<C-j>", "<C-w>j")
+map("n", "<C-k>", "<C-w>k")
+map("n", "<C-l>", "<C-w>l")
+
+map("v", ">", ">gv", { noremap = true })
+map("v", "<", "<gv", { noremap = true })
+
+map("n", "<C-d>", "<C-d>zz", { noremap = true, silent = true })
+map("n", "<C-u>", "<C-u>zz", { noremap = true, silent = true })
+
+map("n", "n", "nzz", { noremap = true, silent = true })
+map("n", "N", "Nzz", { noremap = true, silent = true })
+
 --> LspConfig
 map("n", "gd", vim.lsp.buf.definition)
 map("n", "gD", vim.lsp.buf.declaration)
@@ -13,7 +33,7 @@ map("n", "<leader>ca", vim.lsp.buf.code_action)
 map("n", "<leader>e", vim.diagnostic.open_float)
 
 --> Nonels
-map("n", "<leader>ff", vim.lsp.buf.format, {})
+map("n", "<leader>ff", vim.lsp.buf.format)
 
 --> Cmp
 exported.cmp = {
@@ -22,6 +42,9 @@ exported.cmp = {
 	select_prev_item = "<C-k>",
 	confirm = "<CR>",
 }
+
+--> NeoTree
+map("n", "<C-e>", ":Neotree filesystem toggle left <CR>")
 
 --> Flash
 exported.flash = {
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -4,7 +4,16 @@ vim.cmd "set softtabstop=4"
 vim.cmd "set shiftwidth=4"
 vim.cmd "set scrolloff=10"
 
+vim.cmd "cnoreabbrev Q q"
+vim.cmd "cnoreabbrev Qa qa"
+vim.cmd "cnoreabbrev QA qa"
+
 vim.opt.swapfile = false
+
+vim.g.mapleader = " "
+
+vim.opt.number = true
+vim.o.relativenumber = true
 
 vim.cmd "set clipboard+=unnamedplus"
 vim.g.clipboard = {
@@ -19,38 +28,3 @@ vim.g.clipboard = {
     },
     cache_enabled = 0,
 }
-
-vim.g.mapleader = " "
-
-vim.opt.number = true
-vim.o.relativenumber = true
-
-vim.api.nvim_set_keymap("n", "<C-d>", "<C-d>zz", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<C-u>", "<C-u>zz", { noremap = true, silent = true })
-
-vim.api.nvim_set_keymap("n", "n", "nzz", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "N", "Nzz", { noremap = true, silent = true })
-
--- vim.api.nvim_create_autocmd("LspAttach", {
---     group = vim.api.nvim_create_augroup("lsp", { clear = true }),
---     callback = function(args)
---         vim.api.nvim_create_autocmd("BufWritePre", {
---             buffer = args.buf,
---             callback = function()
---                 vim.lsp.buf.format { async = false, id = args.data.client_id }
---             end,
---         })
---     end,
--- })
-
-local map = vim.keymap.set
-map("i", "<C-h>", "<Left>", {})
-map("i", "<C-j>", "<Down>", {})
-map("i", "<C-k>", "<Up>", {})
-map("i", "<C-l>", "<Right>", {})
-map("v", ">", ">gv", { noremap = true })
-map("v", "<", "<gv", { noremap = true })
-
-vim.cmd "cnoreabbrev Q q"
-vim.cmd "cnoreabbrev Qa qa"
-vim.cmd "cnoreabbrev QA qa"
